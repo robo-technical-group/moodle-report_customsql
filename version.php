6d4b0880eb356312ec4303d033fb8b3fa0832509
Tim Hunt <T.J.Hunt@open.ac.uk>
2018-03-16 20:29:41 +0000

    test

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018021600;
-$plugin->requires  = 2017051500;
+$plugin->requires  = 2016120500;
 $plugin->component = 'report_customsql';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '3.3 for Moodle 3.3+';
